062db71207743800397f5d340235baf0485afd11
SuryaAdi152 <Hyperrevengers@gmail.com>
2024-01-07 21:50:51 +0800

    tubes

---
+++ database/seeders/edgar@gmail.com.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class edgar@gmail.com extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
